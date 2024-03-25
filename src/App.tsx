3122dc4b2ac004d9256f5158f537133579191381
wavewavewavewave <nikitashkrabov@gmail.com>
2024-03-25 12:29:49 +0300

    connected api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import Search from "./components/Search/Search";
@@ -6,10 +6,12 @@ import {Header} from "./components/Header/Header";
 import {TableData} from "./components/Table/Table";
 import {data} from "./constants/Constants";
 import {DataType} from "./types/Types";
+import {beersAPI} from "./api/beers-api";
 
 function App() {
     const [search, setSearch] = useState('');
     const [beers, setBeers] = useState<DataType[]>(data)
+    const [test, setTest] = useState<DataType[]>([])
 
     const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (!event.target.value) {
@@ -25,12 +27,19 @@ function App() {
             ))
     }
 
+    useEffect(() => {
+        beersAPI.getAllBeers().then(res => {
+            setTest(res.data)
+        })
+    }, [])
+    console.log(test, 'TEST')
+
     return (
         <div>
             <Header/>
             <div style={{padding: '20px'}}>
                 <Search onChange={handleOnChange} value={search}/>
-                <TableData beers={beers}/>
+                <TableData beers={test}/>
             </div>
         </div>
     );
